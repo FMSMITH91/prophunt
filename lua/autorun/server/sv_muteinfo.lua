8590c160486a0471fb46a55f3530fe1b777b0b69
FMSMITH91 <kasey.adam.smith@gmail.com>
2016-10-17 11:22:50 -0500

    Updated to latest prophunt

--- lua/autorun/server/sv_muteinfo.lua
+++ lua/autorun/server/sv_muteinfo.lua
@@ -5,3 +5,6 @@ util.AddNetworkString("MuteInfo")
 -- when a player is really lazy and had to use manual/FastDL download:
 resource.AddSingleFile("materials/vgui/hud/wlv_bren.vmt")
 resource.AddSingleFile("materials/vgui/hud/wlv_bren.vtf")
+
+-- Ship
+resource.AddWorkshop("417565863")
